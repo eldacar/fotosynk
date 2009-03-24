52bbf76978ebdd06799408284c627f9f67f4d251
eldacar <hnordin@gmail.com>
2009-03-24 22:17:09 +0100

    Updated with new code

--- src/fotosynk/FotoSynk.java
+++ src/fotosynk/FotoSynk.java
@@ -1,42 +1,42 @@
 package fotosynk;
 
 import org.apache.commons.net.ftp.FTPClient;
-import org.apache.commons.net.ftp.FTPFile;
 import org.apache.commons.net.ftp.FTPReply;
 import java.net.SocketException;
 import java.io.IOException;
 
 public class FotoSynk {
 
+    private static String username;
+    private static String password;
+
     public FotoSynk() {
-	FTPClient f = new FTPClient();
-	f.enterLocalPassiveMode();
-      	try {
-      	    System.out.println("Connecting to storegate");
-	    f.connect("ftp.storegate.se");
-	    f.enterRemotePassiveMode();
-	    int reply = f.getReplyCode();
-	    if (!FTPReply.isPositiveCompletion(reply)) {
-		f.disconnect();
-		System.out.println("Refused connection. Code: " + reply);
-		return;
-	    }
-	    System.out.println("Logging in");
-	    f.login("segesten", "flodhast");
-	    System.out.println("Current dir is:" + f.printWorkingDirectory());
-	    //for(FTPFile file: f.listFiles()) {
-	    //System.out.println(file.getName());
-	    //}
-	    
-	} catch (SocketException e) {
-	    e.printStackTrace();
-	} catch (IOException e) {
-	    e.printStackTrace();
-	}
+        FTPClient f = new FTPClient();
+        f.enterLocalPassiveMode();
+        try {
+            System.out.println("Connecting to storegate");
+            f.connect("ftp.storegate.se");
+            f.login(username, password);
+            int reply = f.getReplyCode();
+            if (!FTPReply.isPositiveCompletion(reply)) {
+                f.disconnect();
+                System.out.println("Refused connection. Code: " + reply);
+                return;
+            }
+            System.out.println("Logging in");
+            System.out.println("Current dir is:" + f.printWorkingDirectory());
+        } catch (SocketException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     public static void main(String[] args) {
-	FotoSynk f = new FotoSynk();
+        if (args.length == 2) {
+            username = args[0];
+            password = args[1];
+        }
+        FotoSynk f = new FotoSynk();
     }
-
 }
